632ca98015b3d593a9ae4465f7272674e42e4f57
Vincent <lichenlu36@gmail.com>
2026-09-14 16:57:53 +0800

    Complete Django environment configuration

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
@@ -15,5 +18,6 @@ os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.dev"),
 )
+
 
 application = get_wsgi_application()
